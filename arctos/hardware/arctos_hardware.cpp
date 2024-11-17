4a7c3fdd00b4c54631616e1ebd9aae9924a11d45
Hashnu <54480342+hashnu@users.noreply.github.com>
2024-11-17 18:14:33 +0100

    Position controller ok

--- arctos/hardware/arctos_hardware.cpp
+++ arctos/hardware/arctos_hardware.cpp
@@ -18,21 +18,16 @@ namespace arctos
 
 const std::vector<double> gear_ratios = {13.5,-150.,150.,48.,67.82,67.82};
 const double encoder_resolution = 2*M_PI/(16384);
+const double joint_rotation_speed_rpm = 1./5;
+const uint8_t motor_acceleration = 1;
+const int can_timeout = 100;
 
 int get_CRC(int id, const std::vector<uint8_t>& data) {
     int crc = id + std::accumulate(data.begin(), data.end(), 0);
     return crc & 0xFF;
 }
 
-bool check_CRC(int id, const std::array<std::uint8_t, 8> & data) {
-    int sum = id;
-    for (size_t i = 0; i < data.size() - 1; ++i) {
-        sum += data[i];
-    }
-    return data.back() == (sum & 0xFF);
-}
-
-bool check_CRC_4(int id, const std::vector<std::uint8_t>  & data) {
+bool check_CRC(int id, const std::vector<std::uint8_t> & data) {
     int sum = id;
     for (size_t i = 0; i < data.size() - 1; ++i) {
         sum += data[i];
@@ -61,7 +56,7 @@ int64_t vectorToSignedInt46(const std::vector<std::uint8_t>& vec) {
 std::vector<uint8_t> int32ToByteArray(int32_t value) {
     std::vector<uint8_t> byteArray(4); // 4 bytes for a 32-bit integer
 
-    if (value<0) {value = -value+8388607};
+    //if (value<0) {value = -value+8388607;}
 
     // Store each byte in big-endian order
     byteArray[0] = static_cast<uint8_t>((value >> 16) & 0xFF);
@@ -71,6 +66,19 @@ std::vector<uint8_t> int32ToByteArray(int32_t value) {
     return byteArray;
 }
 
+std::vector<uint8_t> motorSpeedToByteArray(double speed) {
+    std::vector<uint8_t> byteArray(2);
+
+    if (speed<0) {speed = -speed;}
+    if (speed>3000) {speed=3000.;}
+    
+    int32_t speed_int = std::round(speed);
+
+    byteArray[0] = static_cast<uint8_t>((speed_int >> 8) & 0xFF);
+    byteArray[1] = static_cast<uint8_t>(speed_int & 0xFF);
+
+    return byteArray;
+}
 
 CallbackReturn RobotSystem::on_init(const hardware_interface::HardwareInfo & info)
 {
@@ -137,8 +145,6 @@ std::vector<hardware_interface::CommandInterface> RobotSystem::export_command_in
 return_type RobotSystem::read(const rclcpp::Time & /*time*/, const rclcpp::Duration & period)
 {
 
-
-      //setvbuf(stdout, NULL, _IONBF, BUFSIZ);
     boost::asio::io_context ioc;
     // Retrieve the interface index from the interface name
     const auto idx = canary::get_interface_index("can0");
@@ -155,9 +161,18 @@ return_type RobotSystem::read(const rclcpp::Time & /*time*/, const rclcpp::Durat
     } cFrame;
 
     cFrame = {};
+    uint32_t bit_mask = 0x01;
 
-    for (int joint_id = 1; joint_id <= 4 ; joint_id++)
+    for ( int joint_id = 1; joint_id <= 4 ; joint_id++)
     {
+
+      sock.set_option(canary::filter_if_any{{{
+      canary::filter{}
+        .id_mask(~joint_id)            // Only Joint ID.
+        //.remote_transmission(false) // Only remote transmission frames.
+        .extended_format(false)    // Only standard format frames.
+      }}});
+
       // Get position
       std::vector<uint8_t> command = {0x31};
       int crc = get_CRC(joint_id, command);
@@ -167,20 +182,31 @@ return_type RobotSystem::read(const rclcpp::Time & /*time*/, const rclcpp::Durat
 
       cFrame.header.id(joint_id);
       cFrame.header.payload_length(boost::asio::buffer_copy(
-        canary::net::buffer(cFrame.payload), boost::asio::buffer(command_str)));
+      canary::net::buffer(cFrame.payload), boost::asio::buffer(command_str)));
       sock.send(canary::net::buffer(&cFrame, sizeof(cFrame)));
-      
-      // Add timeout
-      sock.receive(canary::net::buffer(&cFrame, sizeof(cFrame)));
+    
+      while (true) {        
+        sock.receive(canary::net::buffer(&cFrame, sizeof(cFrame)));
 
-      if (check_CRC(joint_id,cFrame.payload) && static_cast<int>(cFrame.header.id()) == joint_id) 
-      {
-        std::vector<std::uint8_t> encoder_value_data = {cFrame.payload.begin()+1,cFrame.payload.end()-1};
-        int64_t encoder_value = vectorToSignedInt46(encoder_value_data);
-        double encoder_value_rad = encoder_value*encoder_resolution/gear_ratios[joint_id-1];
-        joint_position_[joint_id-1] = encoder_value_rad;
-        //std::cout<<"Joint " << joint_id << " rotation :" << encoder_value_rad << "\n";
+        std::vector<std::uint8_t> cFrame_payload(cFrame.payload.begin(), cFrame.payload.end());
+
+        if (check_CRC(joint_id,cFrame_payload))
+        {
+          if (cFrame.header.payload_length() == 8 && static_cast<int>(cFrame.payload[0]) == 49)
+          {
+            std::vector<std::uint8_t> encoder_value_data = {cFrame.payload.begin()+1,cFrame.payload.end()-1};
+            int64_t encoder_value = vectorToSignedInt46(encoder_value_data);
+            double encoder_value_rad = encoder_value*encoder_resolution/gear_ratios[joint_id-1];
+            joint_position_[joint_id-1] = encoder_value_rad;
+            break;
+            //std::cout<<"Joint " << joint_id << " rotation :" << encoder_value_rad << "\n";
+          }
+        }
+        break;
       }
+        //} else {break;}
+        //std::this_thread::sleep_for(std::chrono::microseconds(10));
+      //}
 
       // Get velocity
       std::vector<uint8_t> command2 = {0x32};
@@ -190,21 +216,27 @@ return_type RobotSystem::read(const rclcpp::Time & /*time*/, const rclcpp::Durat
       cFrame.header.extended_format(false);
 
       cFrame.header.id(joint_id);
-      cFrame.header.payload_length(boost::asio::buffer_copy(
-        canary::net::buffer(cFrame.payload), boost::asio::buffer(command_str2)));
+      cFrame.header.payload_length(boost::asio::buffer_copy(canary::net::buffer(cFrame.payload), boost::asio::buffer(command_str2)));
       sock.send(canary::net::buffer(&cFrame, sizeof(cFrame)));
       
-      // Add timeout
-      sock.receive(canary::net::buffer(&cFrame, sizeof(cFrame)));
+      while (true) {        
+          sock.receive(canary::net::buffer(&cFrame, sizeof(cFrame)));
 
-      std::vector<std::uint8_t> cFrame_payload_4 = {cFrame.payload.begin(),cFrame.payload.begin()+4};
-      if (check_CRC_4(joint_id,cFrame_payload_4)) 
-      {
-        int16_t velocity_value = (static_cast<int16_t>(cFrame_payload_4[1]) << 8) | cFrame_payload_4[2];
-        double velocity_value_rad = velocity_value*2*M_PI/60/gear_ratios[joint_id-1];
-        joint_velocities_[joint_id-1] = velocity_value_rad;
-      }
-      
+          std::vector<std::uint8_t> cFrame_payload(cFrame.payload.begin(), cFrame.payload.end());
+
+          if (check_CRC(joint_id,cFrame_payload))
+          {
+            if (cFrame.header.payload_length() == 4 && static_cast<int>(cFrame.payload[0]) == 49)
+            {
+              int16_t velocity_value = (static_cast<int16_t>(cFrame_payload[1]) << 8) | cFrame_payload[2];
+              double velocity_value_rad = velocity_value*2*M_PI/60/gear_ratios[joint_id-1];
+              joint_velocities_[joint_id-1] = velocity_value_rad;
+              break;
+              //std::cout<<"Joint " << joint_id << " rotation :" << encoder_value_rad << "\n";
+            }
+          } else {break;}
+          std::this_thread::sleep_for(std::chrono::microseconds(10));
+      }      
   }
 
   for (auto i = 4; i < joint_velocities_command_.size(); i++)
@@ -224,6 +256,9 @@ return_type RobotSystem::read(const rclcpp::Time & /*time*/, const rclcpp::Durat
 return_type RobotSystem::write(const rclcpp::Time &, const rclcpp::Duration &)
 {
 
+  setvbuf(stdout, NULL, _IONBF, BUFSIZ);
+
+  //std::cout << "X joint command:" << joint_position_command_[0] << "\n";
   std::vector<double> gear_ratios = {13.5,-150.,150.,48.,67.82,67.82};
   double encoder_resolution = 2*M_PI/(16384);
 
@@ -244,22 +279,44 @@ return_type RobotSystem::write(const rclcpp::Time &, const rclcpp::Duration &)
 
   cFrame = {};
 
-  for (auto i = 0; i < 4;i++)//joint_position_command_.size(); i++)
-  {
+  if (joint_position_command_.size() > 0) {
+    for (int joint_id = 1; joint_id <= 4;joint_id++)//joint_position_command_.size(); i++)
+    {
 
-    double encoder_value_double = encoder_value_rad*gear_ratios[joint_id-1]/encoder_resolution;
-    int32_t encoder_int = static_cast<int32_t>(std::round(encoder_value));
-    std::vector<uint8_t> encoder_byte = int32ToByteArray(encoder_int);
+      double encoder_value_double = joint_position_command_[joint_id-1]*gear_ratios[joint_id-1]/encoder_resolution;
+      int32_t encoder_int = static_cast<int32_t>(std::round(encoder_value_double));
+      std::vector<uint8_t> encoder_byte = int32ToByteArray(encoder_int);
 
+      double motor_speed_rpm = joint_rotation_speed_rpm*gear_ratios[joint_id-1];
+      std::vector<uint8_t> motor_speed = motorSpeedToByteArray(motor_speed_rpm);
 
-    int joint_position_command_[i]
+      std::vector<uint8_t> command = {0xF5,motor_speed[0],motor_speed[1],motor_acceleration,encoder_byte[0],encoder_byte[1],encoder_byte[2]};
+      command.push_back(get_CRC(joint_id, command));
 
-            step_in_degree = 1
-        pulses = round(step_in_degree/degre_per_pulses) 
+      std::string command_str(reinterpret_cast<char*>(command.data()), command.size());
+      
+      cFrame.header.id(joint_id);
+      cFrame.header.payload_length(boost::asio::buffer_copy(canary::net::buffer(cFrame.payload), boost::asio::buffer(command_str)));
+      sock.send(canary::net::buffer(&cFrame, sizeof(cFrame)));
+
+      /*
+      if (joint_id == 1) {
+      std::cout << "Command: ";
+      std::cout << std::hex << std::setw(2) << std::setfill('0') << static_cast<int>(command[0]) << " - ";
+      std::cout << std::hex << std::setw(2) << std::setfill('0') << static_cast<int>(command[1]) << " - ";
+      std::cout << std::hex << std::setw(2) << std::setfill('0') << static_cast<int>(command[2]) << " - ";
+      std::cout << std::hex << std::setw(2) << std::setfill('0') << static_cast<int>(command[3]) << " - ";
+      std::cout << std::hex << std::setw(2) << std::setfill('0') << static_cast<int>(command[4]) << " - ";
+      std::cout << std::hex << std::setw(2) << std::setfill('0') << static_cast<int>(command[5]) << " - ";
+      std::cout << std::hex << std::setw(2) << std::setfill('0') << static_cast<int>(command[6]) ;
+      std::cout << "\n";}
+      */
+
 
   }
-
-
+  
+  }
+  
 
   return return_type::OK;
 }
